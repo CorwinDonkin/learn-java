ba098e429995f1ded1f4865c25b1033b92e8d238
CorwinDonkin <55019806+CorwinDonkin@users.noreply.github.com>
2019-09-07 13:08:47 +0100

    Empty project

---
+++ LearnJava/src/main/java/com/donkin/learnjava/SimpleStarting.java
@@ -0,0 +1,21 @@
+package com.donkin.learnjava;
+
+/**
+ *
+ * @author james
+ */
+
+public class SimpleStarting 
+{
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World");
+        for (int i = 0; i<10; i++)
+        {
+            System.out.println("This is something");
+        }
+ 
+    }
+    
+}
