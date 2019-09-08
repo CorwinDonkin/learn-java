1ecac2ac5b8dfa7fa069096b21a31788fc72ca64
CorwinDonkin <55019806+CorwinDonkin@users.noreply.github.com>
2019-09-08 11:23:58 +0100

    Update SimpleStarting.java

--- LearnJava/src/main/java/com/donkin/learnjava/SimpleStarting.java
+++ LearnJava/src/main/java/com/donkin/learnjava/SimpleStarting.java
@@ -11,10 +11,15 @@ public class SimpleStarting
     public static void main(String[] args) 
     {
         System.out.println("Hello World");
-        for (int i = 0; i<10; i++)
+        for (int i = 0; i<10; i = i + 2)
         {
-            System.out.println("This is something");
+            if (i == 4)
+            {
+                System.out.println("Hello number 4");
+            }
+            System.out.println("This is something " + i);
         }
+        
  
     }
     
